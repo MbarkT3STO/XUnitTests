3767ab2c40b9741e2ba5dc2d5ca45035ae7f0b97
mbarkt3sto <mbark1236@gmail.com>
2023-06-06 07:39:01 +0100

    update 26 files

--- App.Tests/CalculatorTests.cs
+++ App.Tests/CalculatorTests.cs
@@ -8,16 +8,32 @@ namespace App.Tests;
 
 public class CalculatorTests
 {
-    [Fact]
-    public void Add_WhenCalled_ReturnsTheSumOfArguments()
-    {
-        // Arrange
-        var calculator = new Calculator();
+	[Fact]
+	public void Add_WhenCalled_ReturnsTheSumOfArguments()
+	{
+		// Arrange
+		var calculator = new Calculator();
 
-        // Act
-        var result = calculator.Add(45, 20);
+		// Act
+		var result = calculator.Add(45, 20);
 
-        // Assert
-        Assert.Equal(65, result);
-    }
+		// Assert
+		Assert.Equal(65, result);
+	}
+
+	[Theory]
+	[InlineData(45, 20, 25)]
+	[InlineData(45, -20, 65)]
+	[InlineData(45, 0, 45)]
+	public void Subtract_WhenCalled_ReturnsTheDifferenceOfArguments(int a, int b, int expected)
+	{
+		// Arrange
+		var calculator = new Calculator();
+
+		// Act
+		var actual = calculator.Subtract(a, b);
+
+		// Assert
+		Assert.Equal(expected, actual);
+	}
 }
